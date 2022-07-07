a365a6b35efdb71a266fb69469feea6fb0db8427
AliceFrost22 <105521104+AliceFrost22@users.noreply.github.com>
2022-07-07 19:02:28 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,16 +1,11 @@
-// "listens" for when the window is scrolled on. When a scroll happens, "scrollFunction" is executed
-$(window).scroll(scrollFunction);
-
-// When the user scrolls down 20px from the top of the document, show the button
-function scrollFunction() {
-  if ( $(window).scrollTop() > 20 ) {
-    $("#go-top-button").show();
-  } else {
-    $("#go-top-button").hide();
-  }
-};
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  $(window).scrollTop(0);
+function PlaySound(audioId) {
+  var thissound=document.getElementById(audioId);
+  thissound.play();
+  console.log('click')
 }
+function StopSound(audioId) {
+  var thissound=document.getElementById(audioId);
+  thissound.pause();
+  thissound.currentTime = 0;
+}
+
